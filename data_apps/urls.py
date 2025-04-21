51df25afcefc09d7b504ea8b44f34df52606ec59
EdzelOrtiz <edzel.ortiz@upr.edu>
2025-04-21 09:13:47 -0400

    questionaire working

--- data_apps/urls.py
+++ data_apps/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('', include('forms.urls')),
+    path('forms/coffee-questionaire/CoffeeQuestionare.html/', views.coffeeTest, name='coffee-questionaire'),
 ]
